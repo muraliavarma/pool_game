b8a246897a754d33e7d167a59beb4d4dae166b01
Murali Varma <muraliavarma@gmail.com>
2012-06-15 19:17:05 -0700

    Initil commit

---
+++ shared/Application.js
@@ -0,0 +1,28 @@
+"use import";
+
+import GC;
+
+exports = Class(GC.Application, function() {
+
+	this._settings = {
+		logsEnabled: window.DEV_MODE,
+		noTimestep: false,
+		showFPS: window.DEV_MODE
+	};
+
+	this.initUI = function() {
+
+	}
+
+	this.launchUI = function() {
+		
+	}
+
+	this.launchSinglePlayerGame = function() {
+		
+	}
+
+	this.launchMultiplayerGame = function() {
+		
+	}
+});
