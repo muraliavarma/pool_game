048ffbbcf1bcb0b51064d54cc08985a3d9816944
Murali Varma <muraliavarma@gmail.com>
2012-06-25 10:31:40 -0700

    Rotating camera

--- shared/World.js
+++ shared/World.js
@@ -5,7 +5,7 @@ import timestep.View as View;
 import timestep.TextView as TextView;
 import timestep.device;
 
-exports = Class(View, function(supr) {
+exports = Class(UIView, function(supr) {
     //class contructor
     this.init = function() {
         supr(this, "init", arguments);
@@ -38,6 +38,8 @@ exports = Class(View, function(supr) {
 			offsetX: 100,
 			offsetY: 100
 		};
+
+		this.isBlueClicked = false;
     }
 
     this.buildView = function() {
@@ -45,24 +47,52 @@ exports = Class(View, function(supr) {
 			parent: this,
 			opacity: 0.5,
 			backgroundColor: "#882222",
-			x: 300,
+			x: 150,
 			y: 300,
 			width: 20,
-			height: 20
+			height: 20,
+			zIndex: 9999
 		});
 
-		this.redRect.onInputSelect = function() {
+		this.redRect.onInputMove = bind(this,function() {
 			this.camera.x += 1;
-		}
+		});
+
+		this.blueRect = new UIView({
+			parent: this,
+			opacity: 0.5,
+			backgroundColor: "#222288",
+			x: 180,
+			y: 300,
+			width: 20,
+			height: 20,
+			zIndex: 9999
+		});
+
+		this.blueRect.onInputStart = bind(this,function() {
+			this.isBlueClicked = true;
+
+		});
+
+		this.blueRect.onInputOut = bind(this,function() {
+			this.isBlueClicked = false;
+		});
 
     }
 
     this.render = function() {
-    	this.ctx.fillStyle = "#0000FF";
+    	if(this.isBlueClicked){
+  	    	//for(var i=0; i < this.world.vertices.length; i++){
+			//	this.world.vertices[i].z += 1;
+			//};
+			this.camera.ry += 0.01;
+			this.camera.z = 400 - Math.cos(this.camera.ry) * 400;
+			this.camera.x = Math.sin(this.camera.ry) * 400;
+		}
+  		this.ctx.fillStyle = "#0000FF";
+		var camera = this.camera;
     	for(var i=0; i < this.world.vertices.length; i++){
-    		var camera = this.camera;
 			var vertex = this.world.vertices[i];
-			this.world.vertices[i].z += 1;
 			var dx = vertex.x - camera.x;
 			var dy = vertex.y - camera.y;
 			var dz = vertex.z - camera.z;
@@ -70,10 +100,10 @@ exports = Class(View, function(supr) {
 			var d1x = Math.cos(camera.ry)*dx + Math.sin(camera.ry)*dz;
 			var d1y = dy;
 			var d1z = Math.cos(camera.ry)*dz - Math.sin(camera.ry)*dx;
-			if(dz > 0){
-				var scale = camera.depth / dz;
-				var posX = scale * dx + camera.offsetX;
-				var posY = scale * dy + camera.offsetY;
+			if(d1z > 0){
+				var scale = camera.depth / d1z;
+				var posX = scale * d1x + camera.offsetX;
+				var posY = scale * d1y + camera.offsetY;
 				var size = scale * 10;
 			}
 
